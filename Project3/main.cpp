c1d56b6c8da863830a352bf98c50ef68898f8369
masterabite <47632668+masterabite@users.noreply.github.com>
2022-12-03 19:46:29 +0300

    Update main.cpp
    Теперь получает изображение с камеры и обрабатывает его по ранее написанной функции draw_lines

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -10,18 +10,15 @@ using namespace cv;
 using namespace cv::cuda;
 using namespace std;
 
-Mat draw_lines(string image_input_name) {
-    std::string image_path = samples::findFile(image_input_name);
-    Mat img, gray, edges;
+void draw_lines(Mat& img, Mat& new_img) {
+    Mat gray, edges;
 
-    img = imread(image_path);
     cvtColor(img, gray, COLOR_BGR2GRAY, 0);
     Canny(gray, edges, 50, 150, 3);
 
     vector<Vec2f> lines;
-    HoughLines(edges, lines, 1, CV_PI / 180, 150, 0, 0);
+    HoughLines(edges, lines, 1, CV_PI / 180, 250, 0, 0);
 
-    Mat new_img;
     img.copyTo(new_img);
 
     for (int i = 0; i < lines.size(); ++i) {
@@ -30,22 +27,27 @@ Mat draw_lines(string image_input_name) {
         double x0 = a * rho, y0 = b * rho;
         Point p1(cvRound(x0 + 2000 * (-b)), cvRound(y0 + 2000 * (a)));
         Point p2(cvRound(x0 - 2000 * (-b)), cvRound(y0 - 2000 * (a)));
-        printf("p1: (%d, %d)\tp2: (%d, %d)\n", p1.x, p1.y, p2.x, p2.y);
         line(new_img, p1, p2, Scalar(0, 255, 0, 0.2), 2, LINE_4);
     }
-    return new_img;
 }
 
 int main() {
-
-    vector<Mat> imgs;
-    int n = 18;
-
-    for (int i = 0; i < n; ++i) {
-        imgs.push_back(draw_lines(format("images/old/image%d.jpg", i)));
-        imwrite(format("images/new/image%d.jpg", i), imgs.back());
+    VideoCapture vid("rtsp://192.168.1.249:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream");
+    {
+        cout << "\ncould not capture\n";
     }
-   
-    waitKey();
+
+    Mat frame, proc_frame;
+    char key = 'a';
+
+    while (true)
+    {
+        vid.grab();
+        vid.retrieve(frame);
+        draw_lines(frame, proc_frame);
+        imshow("Image", frame);
+        imshow("Processed image", proc_frame);
+        key = waitKey(10);
+    }
 	return 0;
 }
