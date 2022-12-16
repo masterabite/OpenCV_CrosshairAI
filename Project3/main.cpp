b8947586f3070a2034e0adef0b2905fb0d0acb8b
masterabite <47632668+masterabite@users.noreply.github.com>
2022-12-16 16:22:33 +0300

    optimization
    +структура, описывающая обертку над прямой на плоскости;
    +алгоритм нахождения точки; пересечения прямых.

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <vector>
 #include <format>
+#include <string>
 
 OPENCV_ALL_HPP
 
@@ -10,44 +11,118 @@ using namespace cv;
 using namespace cv::cuda;
 using namespace std;
 
-void draw_lines(Mat& img, Mat& new_img) {
-    Mat gray, edges;
+struct my_line {
+    double rho, theta;
+    double k, b;
+    Point p1, p2;
+    Point zeroPoint;
+    Point vec;
 
-    cvtColor(img, gray, COLOR_BGR2GRAY, 0);
-    Canny(gray, edges, 50, 150, 3);
+    my_line(Vec2f v) {
+        rho = v[0];
+        theta = v[1];
+        getPoints(2000);
+        vec = p2 - p1;
 
-    vector<Vec2f> lines;
-    HoughLines(edges, lines, 1, CV_PI / 180, 250, 0, 0);
+        k = 1.0 * (p1.y - p2.y) / (p1.x - p2.x);
+        b = 1.0 * p1.y - k * p1.x;
+    }
 
-    img.copyTo(new_img);
+    Point getZeroPoint() {
+        return  Point(cvRound(cos(theta) * rho), cvRound(sin(theta) * rho));
+    }
 
-    for (int i = 0; i < lines.size(); ++i) {
-        float rho = lines[i][0], theta = lines[i][1];
+    pair<Point, Point> getPoints(int k) {
         double a = cos(theta), b = sin(theta);
         double x0 = a * rho, y0 = b * rho;
-        Point p1(cvRound(x0 + 2000 * (-b)), cvRound(y0 + 2000 * (a)));
-        Point p2(cvRound(x0 - 2000 * (-b)), cvRound(y0 - 2000 * (a)));
-        line(new_img, p1, p2, Scalar(0, 255, 0, 0.2), 2, LINE_4);
+        p1 = Point(cvRound(x0 + k * (-b)), cvRound(y0 + k * (a)));
+        p2 = Point(cvRound(x0 - k * (-b)), cvRound(y0 - k * (a)));
+        return make_pair(p1, p2);
     }
+
+    void draw(Mat& img) {
+        auto ps = getPoints(2000);
+        line(img, ps.first, ps.second, Scalar(0, 255, 0, 0.2), 2, LINE_4);
+    }
+};
+
+Point line_crossing(my_line l1, my_line l2) {
+    Point answer;
+    if (l1.k == 0 || l2.k == 0) {
+        answer.y = (l1.k==0? l1.p1.y: l2.p1.y);
+    }
+    if (isinf(l1.k) || isinf(l2.k)) {
+        answer.x = (isinf(l1.k) ? l1.p1.x : l2.p1.x);
+    }
+
+    answer.x = (l2.b - l1.b) / (l1.k - l2.k);
+    answer.y = l1.k*answer.x + l1.b;
+    return answer;
 }
 
-int main() {
-    VideoCapture vid("rtsp://192.168.1.249:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream");
-    {
-        cout << "\ncould not capture\n";
-    }
+void draw_lines_new(Mat& img) {
+    Mat hsv, red, edges;
 
-    Mat frame, proc_frame;
+    cvtColor(img, hsv, COLOR_BGR2HSV, 0);
+    inRange(hsv, Scalar(143, 60, 200), Scalar(179, 256, 256), red);
+
+    vector<Vec2f> lines;
+    HoughLines(red, lines, 1, CV_PI / 180, 300, 0, 0);
+
+    //red.copyTo(img);
+
+    Point pCenter(10, 10);
+    my_line main_line(lines[0]);
+
+    int min_eps = -1;
+    int second_line_ind;
+
+    for (int i = 1; i < lines.size(); ++i) {
+        my_line l(lines[i]);
+                
+        int eps = abs(main_line.vec.x * l.vec.x + main_line.vec.y * l.vec.y);
+        if (eps == 0) {break;}
+        if (eps  < min_eps || min_eps == -1) {
+            second_line_ind = i;
+            min_eps = eps;
+        }
+    }
+    my_line second_line(lines[second_line_ind]);
+
+    //main_line.draw(img);
+    //second_line.draw(img);
+
+    Point cross_point = line_crossing(main_line, second_line);
+    //circle(img, cross_point, 15, Scalar(255, 255, 0));
+
+}
+
+void proc_vid(string streamURL) {
+    VideoCapture vid(streamURL);
+
+    Mat frame;
+
     char key = 'a';
 
     while (true)
     {
         vid.grab();
         vid.retrieve(frame);
-        draw_lines(frame, proc_frame);
+        draw_lines_new(frame);
         imshow("Image", frame);
-        imshow("Processed image", proc_frame);
         key = waitKey(10);
     }
-	return 0;
+}
+
+void proc_img(string imgName) {
+    Mat new_img;
+    Mat img = imread(imgName);
+    draw_lines_new(img);
+    imshow("Image", img);
+    waitKey();
+}
+
+int main() {
+    //proc_img("images/test2.jpg");
+    proc_vid("rtsp://192.168.1.249:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream");
 }
